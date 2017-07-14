461375327d8cd4a123652a5639806f0a07338913
mac <zouc@szmn.com.cn>
2017-07-14 18:01:51 +0800

    新建bsbdj项目

---
+++ LEARNBSBDJ/Classes/Other/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  LEARNBSBDJ
+//
+//  Created by mac on 2017/7/14.
+//  Copyright © 2017年 United Network Services Ltd. of Shenzhen City. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [self initWindow];
+    return YES;
+}
+
+- (void)initWindow{
+    self.window = [[UIWindow alloc]initWithFrame:BS_ScreenF];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[UITabBarController alloc]init];
+}
+
+
+@end
